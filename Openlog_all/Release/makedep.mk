0d63cea25150cd75609a33193b31f47038ddadc4
TobyZhangx <hdzx0410@163.com>
2023-02-26 07:05:30 +0800

    第一次提交

---
+++ Openlog_all/Release/makedep.mk
@@ -0,0 +1,10 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+main.c
+
+src\ASF\common\services\clock\mega\sysclk.c
+
+src\ASF\mega\boards\atmega328p_xplained_mini\init.c
+
